03d080e93b886c9088214a1aee785f37bed8d53e
cromanpa94 <cromanpa94@gmail.com>
2025-10-01 01:06:54 -0700

    Update reconstructions and parameters

--- 3_maxIonRegressions_error.R
+++ 3_maxIonRegressions_error.R
@@ -4,21 +4,23 @@ library(bayclumpr)
 
 Table1 <- read_excel(here("data", "Test 1 - All benthics, epifaunal and infaunal.xlsx"))
 Table1$Material <- rep(1, nrow(Table1))
+colnames(Table1)[c(5, 6, 2, 3, 8)] <- c("D47", "D47error", "Temperature", "TempError", "Ion")
 
-colnames(Table1)[c(6, 5, 2, 3, 8)] <- c("D47", "D47error", "Temperature", "TempError", "Ion")
+# Check basic regression model
+lm(Table1$D47 ~ Table1$Temperature)
 
+# Fit Bayesian regression
 ionmodel <- cal.ion.bayesian(calibrationData = Table1, 
                              IonError = Table1$`Ion error`[1])
+parameters <- data.frame(rstan::summary(ionmodel[[1]])$summary)
+write.csv(parameters, here("results", "parameters_ion.csv"))
 
-model <- cal.bayesian(calibrationData = Table1)
+# Perform reconstructions
+ionRec <- read.csv(here("data", "reconstructions_ion.csv"))
+PredsBay <- rec.bayesian(calModel = ionmodel[[1]],
+                             recData = ionRec,
+                             iter = 1000,
+                             postcalsamples = 100, MC = FALSE)
 
-
-Table1$D47
-Table1$Temperature
-Table1$D47error
-Table1$TempError
-
-plot(Table1$Temperature, Table1$D47)
-
-
+write.csv(PredsBay, here("results", "reconstructed_ion.csv"))
 
